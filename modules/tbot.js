ba6b1ac76efcd84ca6a8ce32820b9d74068f5611
deadlorpa <deadlorpa@gmail.com>
2018-10-22 19:05:21 +0700

    start

--- modules/tbot.js
+++ modules/tbot.js
@@ -5,7 +5,7 @@ var bot = new TelegramBot(token, {polling: true});
 
 function Send(name, email, wallet, hash_fst, hash_lst,score) {
     let result = 'name: '+name+'\ne-mail: '+email+'\nwallet: '+wallet+'\nhash_fst: '+hash_fst+'\nhash_lst: '+hash_lst+'\nscore: '+score;
-    bot.sendMessage( process.env.TG_CH, result);
+    bot.sendMessage( process.env.TG_CHAN, result);
 }
 
 module.exports.Send = Send;
